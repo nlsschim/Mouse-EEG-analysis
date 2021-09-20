4698fa715aeb2c1055344a9b4dbbca06d70e48bc
laurieryan <laurieryan@lauries-air.router>
2021-09-20 16:04:31 -0700

    Adding the new changes so I can restart my slow computer w/o losing them smh

--- median_SHAM.m
+++ median_SHAM.m
@@ -9,19 +9,29 @@ clear all
 file1={'06-30-2020 Mouse Experiment 1/', '06-23-2020 Mouse Experiment 2/', '06-25-2020 Mouse Experiment 1/', '06-24-2020 Mouse Experiment 3/', '06-24-2020 Mouse Experiment 1/', '06-23-2020 Mouse Experiment 1/','05-29-2020 Mouse Experiment 1/'};
 str=string(file1);
 MainDirectory = '/Users/laurieryan/MATLAB/Mourad Lab/Mouse-EEG-analysis github/Data/';
- 
-SHAM_MATRIX = zeros(7,3);
-% sham_stats_matrix = [];
+
+%% medians or variance 
+
+button = input("Create SHAM matrix of median values or variance? '1'=medians, '2'=variance: ") ;
+button2 = input("Matrix of L+US and 2nd LO minus medians minus 1st LO medians? '1' = yes, '2' = no: ") ; 
+normalize = input("Normalize data by median of 1st LO or rms_baseline? '1'=median of 1LO, '2' =rms_baseline: "); 
+
+%% creating SHAM Matrix to store medians or variance 
+
+% SHAM_MATRIX = zeros(7,4);
 figure 
 hold on
+
+%% Reading experiment dates 
+
 for f=1:length(str)
     folder = fullfile(MainDirectory,str{f});
     % dir ('folder');
- 
+
     %% 
-    %Change what is in the string depending on which file\files you want to run
+    %Change what is in the string deSHAMding on which file\files you want to run
     file_list = dir([folder 'TRIAL*.mat']);
-    baseline = dir([folder 'Baseline.mat']); % or baseline 1 or baseline 2 depending on trials 
+    baseline = dir([folder 'Baseline.mat']); % or baseline 1 or baseline 2 deSHAMding on trials 
     disp(length(baseline)) 
     
     if f==6 || f==7
@@ -31,33 +41,33 @@ for f=1:length(str)
     end
     ch_names={'V1L','S1L','S1R', 'V1R', 'lightstim'}; %setting up the names that will be assigned in the matrix and the order
     trial_names={' FIRST LIGHT ONLY' 'LIGHT + US' ' SECOND LIGHT ONLY'};
- 
+
     
 %% Channel configuration 
-    button = 0;
+
     if f==7
     V1L=set_channels(4);S1L=set_channels(3);S1R=set_channels(2);V1R=set_channels(1);lightstim=set_channels(5);
-    button = 7;
     else
     V1L=set_channels(3);S1L=set_channels(4);S1R=set_channels(2);V1R=set_channels(1);lightstim=set_channels(5);
     end
- 
-    %calculate pre-stim RMS for normalization
+
+%% calculate pre-stim RMS for normalization
+
     baseline_rms=[];
     disp(baseline.name);
     load([folder baseline.name])
     calc_baseline;
- 
+
     % create matrix to hold data for statistical testing
     for_stats_new = [];
     for_stats_analysis=[];
-%     kronk=['date_' num2str(f)];
     %insert thresholding here
- 
+
     for z=1:4
-    
-         if isequal(file_list(z).name,"TRIAL 2.mat"), continue, end %for 12-23
- 
+    %      if isequal(file_list(z).name,"TRIAL2.mat"), continue, end % skips trial 2 for refactory period trial does we dont car about (yet)
+    %      if isequal(file_list(z).name,"Trial 2.mat"), continue, end 
+         if isequal(file_list(z).name,"Trial 2.mat"), continue, end %for 12-23
+
     %     counter = counter + 1 ; 
         disp(z)%display the number that the code is on in the terminal, do not put a ';' after it 
         disp(file_list(z).name);%displays the name of the file in the terminal
@@ -69,22 +79,68 @@ for f=1:length(str)
         % % to rename trials and skip 2 
     for_stats_analysis.Trial_2 = for_stats_analysis.Trial_3 ; 
     for_stats_analysis.Trial_3 = for_stats_analysis.Trial_4 ; 
-    
-% sham_stats_matrix = [for_stats_analysis];
 
-    %%Creating a vector to call on later to plot the medians
+%% Creating a vector to call on later to plot the medians
+
+if button ==1 
+    SHAM_FIRST_LIGHT = median(for_stats_analysis.Trial_1);
+    SHAM_LIGHT_ULTRASOUND = median(for_stats_analysis.Trial_2);
+    SHAM_SECOND_LIGHT = median(for_stats_analysis.Trial_3);
+end
+% variance 
+if button == 2 
     SHAM_FIRST_LIGHT = var(for_stats_analysis.Trial_1);
     SHAM_LIGHT_ULTRASOUND = var(for_stats_analysis.Trial_2);
     SHAM_SECOND_LIGHT = var(for_stats_analysis.Trial_3);
-    
-    % minus first light median 
-    SHAMY = [SHAM_FIRST_LIGHT-SHAM_FIRST_LIGHT, SHAM_LIGHT_ULTRASOUND-SHAM_FIRST_LIGHT, SHAM_SECOND_LIGHT-SHAM_FIRST_LIGHT];
-    SHAM_MATRIX(f, :) = [SHAMY] ;
-    plot(1:3, SHAMY, 'o-', 'DisplayName','SHAM DATA')
-    title('SHAM DATA') 
- 
-end
- 
-SHAM_MATRIX
+end 
 
-median_GEN;
+if button2 == 1 
+    % minus first light median/variance
+    SHAMY = [rms_baseline, SHAM_FIRST_LIGHT-SHAM_FIRST_LIGHT, SHAM_LIGHT_ULTRASOUND-SHAM_FIRST_LIGHT, SHAM_SECOND_LIGHT-SHAM_FIRST_LIGHT];
+else
+    % medianL+US and median2LO NOT - median of 1LO
+    SHAMY = [rms_baseline, SHAM_FIRST_LIGHT, SHAM_LIGHT_ULTRASOUND, SHAM_SECOND_LIGHT];
+end
+
+%% filling matrix 
+%     SHAM_MATRIX(f, :) = [SHAMY] ;
+%     plot(1:4, SHAMY, 'o-', 'DisplayName','SHAM DATA')
+%     title('SHAM DATA') 
+
+%% for plotting each experiment data normalized its median of 1st LO 
+
+if f == 1 
+    SHAM_MATRIX{1} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{2} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{3} = [for_stats_analysis.Trial_3] ;
+elseif f == 2 
+    SHAM_MATRIX{4} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{5} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{6} = [for_stats_analysis.Trial_3] ; 
+elseif f == 3 
+    SHAM_MATRIX{7} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{8} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{9} = [for_stats_analysis.Trial_3] ;
+elseif f == 4 
+    SHAM_MATRIX{10} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{11} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{12} = [for_stats_analysis.Trial_3] ;
+elseif f == 5 
+    SHAM_MATRIX{13} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{14} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{15} = [for_stats_analysis.Trial_3] ;
+elseif f == 6
+    SHAM_MATRIX{16} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{17} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{18} = [for_stats_analysis.Trial_3] ;
+elseif f == 7
+    SHAM_MATRIX{19} = [for_stats_analysis.Trial_1] ;
+    SHAM_MATRIX{20} = [for_stats_analysis.Trial_2] ;
+    SHAM_MATRIX{21} = [for_stats_analysis.Trial_3] ;
+end 
+end
+
+%                 m1           m2              m3            m4               m5             m6               m7 
+SHAM{1} = [SHAM_MATRIX{1}; SHAM_MATRIX{4}; SHAM_MATRIX{7}; SHAM_MATRIX{10}; SHAM_MATRIX{13}; SHAM_MATRIX{16}; SHAM_MATRIX{19}] ; % 1LO 
+SHAM{2} = [SHAM_MATRIX{2}; SHAM_MATRIX{5}; SHAM_MATRIX{8}; SHAM_MATRIX{11}; SHAM_MATRIX{14}; SHAM_MATRIX{17}; SHAM_MATRIX{20}] ; % L+US 
+SHAM{3} = [SHAM_MATRIX{3}; SHAM_MATRIX{6}; SHAM_MATRIX{9}; SHAM_MATRIX{12}; SHAM_MATRIX{15}; SHAM_MATRIX{18}; SHAM_MATRIX{21}] ; % 2LO 
--- miniUS_Diag_stim.m
+++ miniUS_Diag_stim.m
@@ -1,11 +1,11 @@
 %% Authors: Kat Floerchinger, Hannah Mach, Henry Tan
 %this code is meant to set the index_stim equal to 60 so that we can
 %standardize the length to calculate reliable p values 
- 
+
 fs=tickrate(1);
 time1=1:dataend(1);
 time=time1/fs/60;
- 
+
 %% change the bandpass for filtering pls
     % default done 
 %     [bb,aa]=butter(3,[3,100]/(fs/2)); %trying to get the us noise out, 3 to 200
@@ -18,52 +18,52 @@ time=time1/fs/60;
 %   [bb,aa]=butter(2,[4,7]/(fs/2));
     % alpha 
 %     [bb,aa]=butter(2,[8,11]/(fs/2));
- 
+
 % bandpasses = [3, 100; 30, 59; 12, 29; 4, 7; 8, 11] ; 
 %     [bb,aa]=butter(2,bandpasses(brain_wave,:)/(fs/2));
- 
+
 %Organize data into structure array
 alldata=[]; %initialize structure array
- 
+
 alldata.V1Ldata=filtfilt(bb,aa,data(datastart(V1L):dataend(V1L))')';
 alldata.S1Ldata=filtfilt(bb,aa,data(datastart(S1L):dataend(S1L))')';
 alldata.S1Rdata=filtfilt(bb,aa,data(datastart(S1R):dataend(S1R))')';
 alldata.V1Rdata=filtfilt(bb,aa,data(datastart(V1R):dataend(V1R))')';
 % alldata.lightstimdata=filtfilt(bb,aa,data(datastart(lightstim):dataend(lightstim))')';
-if button ==7
-    alldata.lightstimdata=filtfilt(bb,aa,data(datastart(5):dataend(5))')'; % for 5/29 
-else
+% if folder == 'C:\Users\Henry\MATLAB\Mourad Lab\Mouse_EEG\Data\SHAM\5-29-20 Mouse Experiment\'
+%     alldata.lightstimdata=filtfilt(bb,aa,data(datastart(5):dataend(5))')'; % for 5/29 
+% else
     alldata.lightstimdata=data(datastart(lightstim):dataend(lightstim));
-end 
- 
- 
+% end 
+
+
 %create names to access fields of 'alldata' for plotting loops
 names={'V1Ldata','S1Ldata','S1Rdata','V1Rdata','lightstimdata'}; 
 foranalysis={'V1Ldata','S1Ldata','S1Rdata','V1Rdata'}; 
- 
+
 %Filter out noise
 % alldata.V1Ldata=alldata.V1Ldata(abs(alldata.V1Ldata)<0.02); %hardcoded filtering
 %% detect stimuli
- 
+
 X = alldata.lightstimdata;
 X=X-min(X);
 X=X/max(X);
 Y=X>0.5;
 Z=diff(Y);
 index_allstim=find(Z>0.5);index_allstim=index_allstim+1;
- 
+
 %find first pulse of each train, if stimulation contains trains
 index_trains=diff(index_allstim)>2*fs; 
 index_allstim(1)=[];
 index_stim=index_allstim(index_trains);
- 
- 
+
+
 %% Create STAs
 for i=1:4 %initiate data array to hold STAs
 stas.(char(names(i)))=[];
        % title(file_list(z).name,'interpreter','none');
 end
- 
+
 % if  time_series ==3 
 %     tb=1; %time before stim to start STA
 %     ta = 3; % first 3 seconds
@@ -71,71 +71,70 @@ end
     tb=1;
     ta=10; %time after stim to end STA
 % end 
- 
+
 %% Data conditioning (cont.) 
 % prevents errors based on discrepency between V1Ldata and
 % lightstimdata length
- 
-% for i=1:4  
-% %     %inner_loop_size = 0;
-% % %     if (fix(length(alldata.V1Ldata)/tickrate/10)) < length(index_stim)
-% % %         inner_loop_size = (fix(length(alldata.V1Ldata)/tickrate/10)-1);
-% % %     else
-% % %         inner_loop_size = length(index_stim)-1;
-% % %     end  
-% % %     for j=2:inner_loop_size %(length(index_stim)-1) %cycle through stimuli
-% 
-% %        for j=2:(length(index_stim)-1)
-% for j=2:(length(index_stim)-2) % to compensate for data chopping so data
-% % vectors are long enough (supposed to be 60 entries) for
-% % for j=2:(length(index_stim)-3) % for 6/25/20 mouse experiment 1,  6/24/20
-% % experiment 1
-% %         for j=2:(length(index_stim)-4)
-% %      for j=2:(length(index_stim)-5) % 6/24/20 experiment 3 
-%         stas.(char(names(i)))=[stas.(char(names(i))); alldata.(char(names(i)))((index_stim(j)-fs*tb):(index_stim(j)+fs*ta))];
-% 
-%     end
-% end
- 
- 
+
 for i=1:4
     try 
-        for j =2:(length(index_stim)-2) 
+        for j =2:(length(index_stim)-1) 
             stas.(char(names(i)))=[stas.(char(names(i))); alldata.(char(names(i)))((index_stim(j)-fs*tb):(index_stim(j)+fs*ta))];
         end
     catch 
         warning('Index exceeds the number of array elements. Trying j=2:(length(index_stim)-3)') 
-        for j =2:(length(index_stim)-3) 
+        for j =2:(length(index_stim)-2) 
             stas.(char(names(i)))=[stas.(char(names(i))); alldata.(char(names(i)))((index_stim(j)-fs*tb):(index_stim(j)+fs*ta))];
         end
     end 
 end 
- 
+
 %% plot STAS
- 
+
 responseWindowEnd=0.4;
- 
+
 %figure
 x2=1:length(stas.(char(names(1))));
 x2=x2/fs-1;%time axis
 %ylabels={'S1 (mV)';'A1 (mV)';'V1R (mV)'; 'V1L (mV)';'A1R (Hz)';'S1R (Hz)'};
- 
+
 minvalarray=zeros(length(foranalysis),1);
 minidxarray=zeros(length(foranalysis),1);
 maxvalarray=zeros(length(foranalysis),1);
 maxidxarray=zeros(length(foranalysis),1);
 RMSvalbarray=zeros(length(foranalysis),1);
 RMSvalaarray=zeros(length(foranalysis),1);
- 
- 
+
+
 % end
- 
+
 %%
 %collect all of the individual points of data
 all_points(1).name=names(1);
 d=stas.(char(names(1)));
- 
 
+
+%filter data
+% d=filtfilt(bb,aa,d')';
+
+% max_rms=0;
+% if time_series == 3 
+%     for k=1:3 
+%        concat=['RMSvals_' num2str(k)];
+% %        all_points(1).(concat)=rms(alldata.(char(names))(:,fs*(tb+k-1):fs*(tb+k))');
+%        all_points(1).(concat)=rms(d(:,fs*(tb+k-1):fs*(tb+k))');
+% %        plot(d(:,fs*(tb+k-1):fs*(tb+k)))
+% %        maxrms=max(all_points(1).(concat));
+% %        if maxrms > max_rms
+% %            max_rms=maxrms;
+% %        end
+%  
+%     end 
+%     % % for 0.5 seconds increments 
+%     all_points(1).RMSvals_0point5=rms(d(:,fs*(tb+0.5-1):fs*(tb+0.5))');   
+% 	all_points(1).RMSvals_1point5=rms(d(:,fs*(tb+1.5-1):fs*(tb+1.5))');   
+%     all_points(1).RMSvals_2point5=rms(d(:,fs*(tb+2.5-1):fs*(tb+2.5))');  
+% else 
    for k=1:10
        concat=['RMSvals_' num2str(k)];
 %        all_points(1).(concat)=rms(alldata.(char(names))(:,fs*(tb+k-1):fs*(tb+k))');
@@ -147,48 +146,53 @@ d=stas.(char(names(1)));
 %        end
    end 
 % end 
- 
 
+%       
+%  
+% % first 3 sec
+% if time_series == 3 
+%     matrix=[all_points(1).RMSvals_0point5; all_points(1).RMSvals_1; all_points(1).RMSvals_1point5; all_points(1).RMSvals_2; 
+%     all_points(1).RMSvals_2point5; all_points(1).RMSvals_3]; 
+% else 
     matrix=[all_points(1).RMSvals_1; all_points(1).RMSvals_2; all_points(1).RMSvals_3; all_points(1).RMSvals_4; all_points(1).RMSvals_5;
     all_points(1).RMSvals_6; all_points(1).RMSvals_7; all_points(1).RMSvals_8; all_points(1).RMSvals_9; all_points(1).RMSvals_10];
 % end 
- 
-%normalize the data using the baseline RMS
-matrix=matrix/rms_baseline;
- 
- 
- 
- 
+
+% duplicated here from "arrange data for statistical analysis"
+% in order to normalize matrix by median of 1st LO (trial 1)
+% matrix is unchanged 
+
+fakes=size(matrix);
+fakeS=fakes(1)*fakes(2);
+fakefor_stats=reshape(matrix,1,fakeS);
+% fakeconc=['Trial_' num2str(z)];
+fakeconc=['Trial_1'];
+fakefor_stats_analysis.(fakeconc)=fakefor_stats;
+
+
+% normalize the data using the baseline RMS
+if normalize == 2 
+    matrix=matrix/rms_baseline;
+elseif normalize == 1
+    matrix=matrix/(median(fakefor_stats_analysis.Trial_1));
+end 
+
 %% calculate z-scores
-[z_scores,mu,sigma]=find_zscores(matrix, baseline_rms);
- 
+% [z_scores,mu,sigma]=find_zscores(matrix, baseline_rms);
+
 %% arrange data for statistical analysis
- 
-% reshape rms matrix into a vector to be used to statistical testing
-s=size(matrix);
-S=s(1)*s(2);
-for_stats=reshape(matrix,1,S);
+% values for for_stats_analysis are reset by normalized values 
+
+% % reshape rms matrix into a vector to be used to statistical testing
+s=size(matrix); % 10 by 22 matrix 
+S=s(1)*s(2); % S = 10*22 = 220 
+for_stats=reshape(matrix,1,S); % turns 10x22 matrix into 1 by 220 vector
 conc=['Trial_' num2str(z)];
 for_stats_analysis.(conc)=for_stats;
- 
- 
+
+
 % after dividing data into normal distribution: 
 % >mean+4*stddev)(dataset) = [] 
 deviation=std(for_stats_analysis.(conc));
 trialmean = mean(for_stats_analysis.(conc));
 for_stats_analysis.(conc) = for_stats_analysis.(conc)(for_stats_analysis.(conc)<trialmean+4*deviation);
-
-%%IN ORDER TO COMPARE EVERY POINT IN STATS TEST
-% reshape rms matrix into a vector to be used to statistical testing
-% s=size(matrix);
-% S=s(1)*s(2);
-% for_stats=reshape(matrix,1,S);
-% conc=['Trial_' num2str(z)];
-% for_stats_analysis.(conc).(kronk)=for_stats;
-%  
-%  
-% % after dividing data into normal distribution: 
-% % >mean+4*stddev)(dataset) = [] 
-% deviation=std(for_stats_analysis.(conc).(kronk));
-% trialmean = mean(for_stats_analysis.(conc).(kronk));
-% for_stats_analysis.(conc).(kronk) = for_stats_analysis.(conc).(kronk)(for_stats_analysis.(conc).(kronk)<trialmean+4*deviation);
